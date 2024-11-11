08b844925225b8d3cf5932b6d3cfdee62237a434
Jacopo Grassi <jacopo.grassi@polito.it>
2024-11-11 18:46:04 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,4 +1,9 @@
 import streamlit as st
 import sys
 sys.path.append('packages')
+
+# Set the page configuration
+st.set_page_config(page_title='ClimALM', page_icon='🌍', layout='wide')
+st.title('ClimALM')
+
 st.subheader('Collection of streamlit dashboard with implementation of Climate Services tools powered by AI')
