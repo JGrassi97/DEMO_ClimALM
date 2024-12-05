23bc257e8a30482012cd56eb8af2af94933a8733
Jacopo Grassi <jacopo.grassi@polito.it>
2024-12-05 14:51:47 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -4,16 +4,18 @@ import time
 
 sys.path.append('packages')
 
+# Set the page configuration
+st.set_page_config(page_title='ClimALM', page_icon='🌍', layout='wide')
+
 # Inizializza il tempo all'avvio dell'app
 if "start_time" not in st.session_state:
     st.session_state.start_time = time.time()
 
 # Calcola il tempo di utilizzo
 elapsed_time = time.time() - st.session_state.start_time
-st.write(f"Tempo di utilizzo dell'app: {elapsed_time:.2f} secondi")
 
-# Set the page configuration
-st.set_page_config(page_title='ClimALM', page_icon='🌍', layout='wide')
 st.title('ClimALM')
 
 st.subheader('Collection of streamlit dashboard with implementation of Climate Services tools powered by AI')
+
+st.write(f"Tempo di utilizzo dell'app: {elapsed_time:.2f} secondi")
